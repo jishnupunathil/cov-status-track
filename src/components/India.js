0efec85a103ac5ac948eff771ecd050aaf7cbd86
Jishnu <jishnupunathil000@gmail.com>
2023-07-11 15:56:35 +0530

    data listed on table

--- src/components/India.js
+++ src/components/India.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Card } from "react-bootstrap";
+import StateData from "./StateData";
+
 
 class India extends Component {
   render() {
@@ -9,69 +11,52 @@ class India extends Component {
           <img
             src="https://www.countryflags.com/wp-content/uploads/india-flag-png-large.png "
             alt=""
-            style={ {width:'3rem'} }
+            style={{ width: "3rem" }}
           />
           <h3>INDIA</h3>
         </div>
         <div className="col-md-12">
           <div className="row">
             <div className="col-md-3">
-
-            <Card bg="primary" style={{ width: '18rem' }}>
-      <Card.Body className="text-center">
-        <Card.Title>Total Case</Card.Title>
-        <h3>19767</h3>
-        <Card.Text>
-          [Today:25]
-        </Card.Text>
-
-      </Card.Body>
-    </Card>
-              
+              <Card bg="primary" style={{ width: "18rem" }}>
+                <Card.Body className="text-center">
+                  <Card.Title>Total Case</Card.Title>
+                  <h3>19767</h3>
+                  <Card.Text>[Today:25]</Card.Text>
+                </Card.Body>
+              </Card>
             </div>
             <div className="col-md-3">
-
-            <Card bg="warning" style={{ width: '18rem' }}>
-      <Card.Body className="text-center">
-        <Card.Title>Active Cases</Card.Title>
-        <h3>19767</h3>
-        <Card.Text>
-          [Today:25]
-        </Card.Text>
-
-      </Card.Body>
-    </Card>
-              
+              <Card bg="warning" style={{ width: "18rem" }}>
+                <Card.Body className="text-center">
+                  <Card.Title>Active Cases</Card.Title>
+                  <h3>19767</h3>
+                  <Card.Text>[Today:25]</Card.Text>
+                </Card.Body>
+              </Card>
             </div>
             <div className="col-md-3">
-
-            <Card bg="success" style={{ width: '18rem' }}>
-      <Card.Body className="text-center">
-        <Card.Title>Recovered</Card.Title>
-        <h3>19767</h3>
-        <Card.Text>
-          [Today:25]
-        </Card.Text>
-
-      </Card.Body>
-    </Card>
-              
+              <Card bg="success" style={{ width: "18rem" }}>
+                <Card.Body className="text-center">
+                  <Card.Title>Recovered</Card.Title>
+                  <h3>19767</h3>
+                  <Card.Text>[Today:25]</Card.Text>
+                </Card.Body>
+              </Card>
             </div>
             <div className="col-md-3">
-
-            <Card bg="danger" style={{ width: '18rem' }}>
-      <Card.Body className="text-center">
-        <Card.Title>Death</Card.Title>
-        <h3>19767</h3>
-        <Card.Text>
-          [Today:25]
-        </Card.Text>
-
-      </Card.Body>
-    </Card>
-              
+              <Card bg="danger" style={{ width: "18rem" }}>
+                <Card.Body className="text-center">
+                  <Card.Title>Death</Card.Title>
+                  <h3>19767</h3>
+                  <Card.Text>[Today:25]</Card.Text>
+                </Card.Body>
+              </Card>
             </div>
           </div>
+        </div>
+        <div>
+            <StateData/>
         </div>
       </div>
     );
--- src/components/StateData.js
+++ src/components/StateData.js
@@ -1,15 +1,108 @@
-import React,{Component} from "react";
+import Axios from "axios";
+import React, { Component } from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
 
-class StateData extends Component{
+class StateData extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stateDate: {},
+    };
+  }
 
-    render(){
+  componentDidMount() {
+    Axios
+      .get("https://data.covid19india.org/state_district_wise.json")
+      .then((res) => {
+        console.log(res);
+        this.setState({ stateDate: res.data });
+      });
+  }
 
-        return(
-            <div>
-            <h1>StateData</h1>
-            </div>
-        )
-    }
+  render() {
+
+    
+
+      let key=Object.keys(this.state.stateDate)
+
+    
+    return (
+      <div className="row">
+        <div className="col-md-12">
+        <Accordion >
+        {
+            key.map((itm,key)=>{
+            
+                let districts=this.state.stateDate[itm].districtData
+                let district_keys=Object.keys(districts)
+
+                let total_active=0;
+                let total_confirmed=0;
+                let total_recovered=0;
+                let total_death=0;
+
+                let districts_list=[]
+                for(let x in districts){
+
+                    total_active+=districts[x].active
+                    total_confirmed+=districts[x].confirmed
+                    total_recovered+=districts[x].recovered
+                    total_death+=districts[x].death
+                    let ob=districts[x]
+                    ob["districr_name"]=x
+                    districts_list.push(ob)
+                    
+                }
+
+                return(
+
+                    <Accordion.Item eventKey={key}>
+                      <Accordion.Header>{itm}</Accordion.Header>
+                      <Accordion.Body>
+                        <table className="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <td>District</td>
+                                    <td>Confirmed</td>
+                                    <td>Active</td>
+                                    <td>Recovered</td>
+                                    <td>death</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                              {
+                                districts_list.map((itm,key)=>{
+                                  return(
+                                    <tr key={key}>
+                                      <td>{itm.districr_name}</td>
+                                      <td>{itm.confirmed}</td>
+                                      <td>{itm.active}</td>
+                                      <td>{itm.recovered}</td>
+                                      <td>{itm.deceased}</td>
+                                    </tr>
+
+                                  )
+                                })
+
+                              }
+                                
+                            </tbody>
+                        </table>
+                      </Accordion.Body>
+                    </Accordion.Item>
+
+                )
+
+
+            })
+            
+            
+        }
+    </Accordion>
+        </div>
+      </div>
+      );
+  }
 }
 
-export default StateData
+export default StateData;
